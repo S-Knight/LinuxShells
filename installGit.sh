75cabb3ac030988d453e7a4b8db9d38d38e6e3be
fuchangjun <fcjailybo@163.com>
2015-11-15 18:05:37 +0800

    添加了几个常用的命令别名

--- installGit.sh
+++ installGit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #源码安装Git2.6.3
 yum -y remove git
-yum -y  install curl-devel expat-devel gettext-devel   openssl-devel zlib-devel
+yum -y  install curl-devel expat-devel gettext-devel   openssl-devel zlib-devel bash-completion
 
 wget https://www.kernel.org/pub/software/scm/git/git-2.6.3.tar.xz
 tar -xvf git-2.6.3.tar.xz
@@ -10,3 +10,11 @@ make configure
 ./configure --prefix=/usr
 make all 
 make install
+
+cp contrib/completion/git-completion.bash /etc/bash_completion.d/
+. /etc/bash_completion
+
+git config --system alias.st status
+git config --system alias.ci commit
+git config --system alias.co checkout
+git config --system alias.br branch
